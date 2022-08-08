274cf3de94f9124c4a6733c591aa3f2ac4376f5b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-08 16:37:51 +0800

    use command - craft subcommand, investigate command

--- TextBased/Program.cs
+++ TextBased/Program.cs
@@ -1,4 +1,5 @@
-﻿Variable variable = new Variable();
+﻿#pragma warning disable IDE0057, CS8600, CS8602
+Variable variable = new Variable();
 void ExplorationEngine(int CurrentTile)
 {
     
@@ -11,6 +12,12 @@ void ExplorationEngine(int CurrentTile)
     Dictionary<int, string> ItemsInLocation = new Dictionary<int, string>();
     string[,,] ItemUsages = new string[,,] { { { "Axe", "You heave the (quite heavy) axe and somehow manage to chop down a tree. Then, you clumsily cut the log in half multiple times until you have several planks.", "You gain: Planks", "Planks", "Obtain Item" }, {"", "", "", "", ""} } };
     ItemsInLocation.Add(0, "Axe");
+    ItemsInLocation.Add(4, "Crafting Kit");
+    Dictionary<string, string> CraftingResults = new Dictionary<string, string>();
+    Dictionary<int, string> InvestigationResults = new Dictionary<int, string>();
+    InvestigationResults.Add(0, "You see an axe lying on the floor of the jungle.");
+    InvestigationResults.Add(4, "You see an open box to the left of your gaze. It appears to hold hammers, nails, and other miscellaneous items.");
+    CraftingResults.Add("Planks", "Boat");
     
     
     string[] locationDesc = new string[] {
@@ -93,7 +100,7 @@ void ExplorationEngine(int CurrentTile)
                 Console.WriteLine("Invalid Direction.");
             }
         }
-
+        Console.WriteLine(locationDesc[CurrentTile]);
     }
     else if (Input.StartsWith("help")) //help command
     {
@@ -146,43 +153,96 @@ void ExplorationEngine(int CurrentTile)
     }
     else if (Input.StartsWith("use")) //use command
     {
-        
-        for (int i = 0; i < 2; i++)
+        if (Input.StartsWith("use craft") && variable.inventory.Contains("Crafting Kit"))
         {
-
-            if (variable.inventory.Contains(ItemUsages[CurrentTile, i, 0]) && Input.Substring(4) == ItemUsages[CurrentTile, i, 0])
-
+            if (variable.inventory.Contains(Input.Substring(10)))
             {
-                switch (ItemUsages[CurrentTile, i, 4])
+                if (CraftingResults.ContainsKey(Input.Substring(10)))
                 {
-                    case "Obtain Item":
-                        string item = ItemUsages[CurrentTile, i, 3];
-                       var isPresentInInventory = variable.GetItem(item);
-                        if (isPresentInInventory == 0)
-                        {
-                            Console.WriteLine(ItemUsages[CurrentTile, i, 1]);
-                        }
-                        
-                        break;
-
+                    Console.WriteLine("You use your crafting kit, banging together some " + Input.Substring(10) + ", and you manage to create a makeshift " + CraftingResults[Input.Substring(10)] + ".");
+                    variable.GetItem(CraftingResults[Input.Substring(10)]);
+                    Console.WriteLine("You have crafted: " + CraftingResults[Input.Substring(10)]);
                 }
-
+                else
+                {
+                    Console.WriteLine("You try, but you simply cannot imagine what you could make with a(n) " + Input.Substring(10) + ".");
+                }
             }
-            else if (!variable.inventory.Contains(ItemUsages[CurrentTile, i, 0]) && Input.Substring(4) == ItemUsages[CurrentTile, i, 0])
+            else
             {
                 Console.WriteLine("You do not have that item.");
             }
-
-
         }
+        else if (!variable.inventory.Contains("Crafting Kit") && Input.StartsWith("use craft"))
+        {
+            if (variable.inventory.Contains(Input.Substring(10)))
+            {
+                Console.WriteLine("You do your best, trying to somehow make an item, but it is futile. Perhaps you need some special equipment to craft.");
 
+            }
+            else
+            {
+                Console.WriteLine("You do not have that item.");
+            }
+           
+        }
+        else
+        {
+            for (int i = 0; i < 2; i++)
+            {
+
+                if (variable.inventory.Contains(ItemUsages[CurrentTile, i, 0]) && Input.Substring(4) == ItemUsages[CurrentTile, i, 0])
+
+                {
+                    switch (ItemUsages[CurrentTile, i, 4])
+                    {
+                        case "Obtain Item":
+                            string item = ItemUsages[CurrentTile, i, 3];
+                            var isPresentInInventory = variable.GetItem(item);
+                            if (isPresentInInventory == 0)
+                            {
+                                Console.WriteLine(ItemUsages[CurrentTile, i, 1]);
+                            }
+
+                            break;
+
+                    }
+
+                }
+                else if (!variable.inventory.Contains(ItemUsages[CurrentTile, i, 0]) && Input.Substring(4) == ItemUsages[CurrentTile, i, 0])
+                {
+                    Console.WriteLine("You do not have that item.");
+                }
+
+
+            }
+        }
+        
+        
+
+    }
+    else if (Input.StartsWith("investigate"))
+    {
+        ItemsInLocation.TryGetValue(CurrentTile, out string itemInLocation);
+        if (!variable.Inventory.Contains(itemInLocation))
+        {
+            Console.WriteLine(InvestigationResults[CurrentTile]);
+        }
+        else if (itemInLocation == null)
+        {
+            Console.WriteLine("You look around; however, you spot nothing out of the ordinary.");
+        }
+        else
+        {
+            Console.WriteLine("You see nothing except the item you already picked up.");
+        }
     }
     else
     {
         Console.WriteLine("Invalid Command or Invalid Version.");
     }
 
-    Console.WriteLine(locationDesc[CurrentTile]);
+    
     ExplorationEngine(CurrentTile);
 
 }
